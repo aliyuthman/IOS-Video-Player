a6d1c3690ade55b5798ae346371797ee68f573da
Aliyu Usman <aliyuusman64@gmail.com>
2023-11-01 06:24:47 +0100

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Aliyu Usman on 01/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
